0c3568d65ee2c7b464e49b3e2b45833ef22553fc
Air123Akash@ <akash.xportsoft@gmail.com>
2023-11-28 16:44:58 +0530

    Order Details and cart screen done

--- lib/Screens/cartscreen.dart
+++ lib/Screens/cartscreen.dart
@@ -1,20 +1,15 @@
-import 'dart:convert';
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:looks_beauty/Screens/home_screen.dart';
+import 'package:looks_beauty/Screens/orderDetails.dart';
 import 'package:looks_beauty/Screens/product_Screen.dart';
 import 'package:looks_beauty/components/appbarforMyAccScreen.dart';
-import 'package:looks_beauty/components/starRating.dart';
 import 'package:looks_beauty/constants/color_constant.dart';
 import 'package:looks_beauty/constants/image_constant.dart';
-import 'package:looks_beauty/constants/string_constant.dart';
-import 'package:provider/provider.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({
-    super.key,
-  });
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<CartScreen> createState() => _CartScreenState();
@@ -57,17 +52,70 @@ class _CartScreenState extends State<CartScreen> {
               ),
             ),
             SliverFillRemaining(
-              hasScrollBody: false,
+              hasScrollBody: true,
               child: Stack(
                 children: [
                   Padding(
                     padding: const EdgeInsets.symmetric(
-                        horizontal: 16, vertical: 28),
-                    child: Align(
-                      alignment: Alignment.bottomCenter,
+                      horizontal: 12,
+                    ),
+                    child: Column(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.all(12),
+                          child: Center(
+                            child: Text(
+                              "Subtotal ₹1100.00",
+                              style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.w600,
+                                color: bottomBarColor,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "Your order is delivering to:",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                              color: bottomBarColor,
+                            ),
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            const Expanded(
+                              child: Text(
+                                "Xportsoft Technologies, Kardhan Road, Khojkipur, Ambala Cantt - 133001",
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ),
+                            TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                "Change",
+                                style: TextStyle(color: bottomBarColor),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Orders(context),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 28,
+                    left: 16,
+                    right: 16,
+                    child: ClipRRect(
                       child: Container(
                         height: 48,
-                        width: size.width,
                         decoration: BoxDecoration(
                           color: baseColor,
                           borderRadius: BorderRadius.circular(20),
@@ -84,54 +132,6 @@ class _CartScreenState extends State<CartScreen> {
                       ),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 12,
-                    ),
-                    child: Column(
-                      children: [
-                        const Padding(
-                          padding: EdgeInsets.all(12),
-                          child: Center(
-                              child: Text(
-                            "Subtotal ₹1100.00",
-                            style: TextStyle(
-                                fontSize: 22,
-                                fontWeight: FontWeight.w600,
-                                color: bottomBarColor),
-                          )),
-                        ),
-                        const Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              "Your order is delivering to:",
-                              style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w500,
-                                  color: bottomBarColor),
-                            )),
-                        Row(
-                          children: [
-                            const Expanded(
-                              child: Text(
-                                "Xportsoft Technologies, Kardhan Road, Khojkipur, Ambala Cantt - 133001",
-                                style: TextStyle(fontSize: 16),
-                              ),
-                            ),
-                            TextButton(
-                                onPressed: () {},
-                                child: const Text(
-                                  "Change",
-                                  style: TextStyle(color: bottomBarColor),
-                                ))
-                          ],
-                        ),
-                        Orders(context),
-                        BestSellerCard(),
-                        // discoverMore(context)
-                      ],
-                    ),
-                  ),
                 ],
               ),
             ),
@@ -142,39 +142,33 @@ class _CartScreenState extends State<CartScreen> {
   }
 
   Widget Orders(context) {
-    Size size = MediaQuery.of(context).size;
     final List brandListImage = [mdImg, mdhairImg];
-    openProductDetailScreen(context) {
-      Navigator.push(context,
-          MaterialPageRoute(builder: (context) => const ProductDetailScreen()));
-    }
 
-    return Container(
-      height: 200,
-      color: brandBackColor,
-      child: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-                semanticChildCount: 2,
-                shrinkWrap: true,
-
-                // physics: NeverScrollableScrollPhysics(),
-                scrollDirection: Axis.vertical,
-                itemCount: brandListImage.length,
-                itemBuilder: (BuildContext cntx, indx) {
-                  return GestureDetector(
-                    onTap: () {
-                      openProductDetailScreen(context);
-                    },
-                    child: Image.asset(
-                      brandListImage[indx],
-                      scale: 2,
-                    ),
-                  );
-                }),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: List.generate(
+        brandListImage.length,
+        (index) => GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => OrderDetail()),
+            );
+          },
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Image.asset(
+                    brandListImage[index],
+                    scale: 2,
+                  ),
+                ],
+              ),
+              Divider()
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
---
+++ lib/Screens/orderDetails.dart
@@ -0,0 +1,219 @@
+import 'package:flutter/material.dart';
+import 'package:looks_beauty/components/appbarforMyAccScreen.dart';
+import 'package:looks_beauty/constants/color_constant.dart';
+import 'package:looks_beauty/constants/image_constant.dart';
+
+class OrderDetail extends StatefulWidget {
+  const OrderDetail({Key? key}) : super(key: key);
+
+  @override
+  State<OrderDetail> createState() => _OrderDetailState();
+}
+
+class _OrderDetailState extends State<OrderDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            automaticallyImplyLeading: false,
+            pinned: true,
+            backgroundColor: safeareaColor,
+            expandedHeight: 50, // Adjust the value as needed
+            flexibleSpace: appbarMyAcc(
+              showCartIcon: true,
+              onTextChange: 'Order Details',
+              title: '', // Add a title if needed
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                OrderIncart(),
+                Text("How’s your item?",
+                    style: TextStyle(
+                        color: bottomBarColor,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600)),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: orderDetailsItem(context),
+                ),
+                Text(
+                  "Order info",
+                  style: TextStyle(
+                      color: bottomBarColor,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: orderInfoItem(context),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Widget OrderIncart() {
+  return Container(
+    decoration: BoxDecoration(color: trendingHimcolor),
+    padding: EdgeInsets.all(16),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Image.asset(
+          mdImg,
+          scale: 3,
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Maybellinne Fit me - Matte+Poreless',
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  'Purchased on 20-Nov-2023',
+                  style: TextStyle(fontSize: 10),
+                ),
+              ],
+            ),
+          ),
+        ),
+        InkWell(
+          onTap: () {},
+          child: Container(
+            height: 30,
+            width: 100,
+            decoration: BoxDecoration(
+              color: baseColor,
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+            child: Center(
+              child: Text(
+                "Buy it again",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        )
+      ],
+    ),
+  );
+}
+
+Widget orderDetailsItem(context) {
+  final List settingOption = [
+    "Write a Product Review",
+    "Create a Video Review",
+  ];
+  final List<Function> onTapFunctions = [
+    () {
+      // Function for the first row
+      // Add your code here
+    },
+    () {
+      // Function for the second row
+      // Add your code here
+    },
+  ];
+
+  return ListView.builder(
+      padding: EdgeInsets.zero,
+      shrinkWrap: true,
+      itemCount: settingOption.length,
+      itemBuilder: (BuildContext cntx, indx) {
+        return Column(
+          children: [
+            GestureDetector(
+              onTap: () {
+                onTapFunctions[indx]();
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: Text(
+                      settingOption[indx],
+                      style: const TextStyle(color: bottomBarColor),
+                    ),
+                  ),
+                  const Icon(
+                    Icons.arrow_forward_ios,
+                    size: 16,
+                  )
+                ],
+              ),
+            ),
+            const Divider(),
+          ],
+        );
+      });
+}
+
+Widget orderInfoItem(context) {
+  final List settingOption = [
+    "View order details",
+    "Download invoice",
+  ];
+  final List<Function> onTapFunctions = [
+    () {
+      // Function for the first row
+      // Add your code here
+    },
+    () {
+      // Function for the second row
+      // Add your code here
+    },
+  ];
+
+  return ListView.builder(
+      padding: EdgeInsets.zero,
+      shrinkWrap: true,
+      itemCount: settingOption.length,
+      itemBuilder: (BuildContext cntx, indx) {
+        return Column(
+          children: [
+            GestureDetector(
+              onTap: () {
+                onTapFunctions[indx]();
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: Text(
+                      settingOption[indx],
+                      style: const TextStyle(color: bottomBarColor),
+                    ),
+                  ),
+                  const Icon(
+                    Icons.arrow_forward_ios,
+                    size: 16,
+                  )
+                ],
+              ),
+            ),
+            const Divider(),
+          ],
+        );
+      });
+}
